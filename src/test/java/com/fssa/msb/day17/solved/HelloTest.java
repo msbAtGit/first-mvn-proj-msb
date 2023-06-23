3f54d36163bb7b4bf4fce0eea4b7e0893a333e48
msbAtGit <95150116+msbAtGit@users.noreply.github.com>
2023-06-23 10:16:26 +0530

    Add files via upload

---
+++ src/test/java/com/fssa/msb/day17/solved/HelloTest.java
@@ -0,0 +1,14 @@
+package com.fssa.msb.day17.solved;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloTest {
+	
+	@Test
+	public void helloTest() {
+		
+		assertTrue(true);
+	}
+}
